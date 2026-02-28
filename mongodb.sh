fa53fcd27caeba22852e9f9f7d61bf2d1b54fa7d
sabeerabegumsayyad-max <sabeerabegumsayyad@gmail.com>
2026-03-01 01:03:37 +0530

    instances are creating

---
+++ mongodb.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+userid=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+logs_folder="/var/log/shallscript-logs"
+scriptname=$(echo $0 | cut -d "." -f1)
+log_file="$logs_folder/$scriptname.log"
+
+mkdir -p $logs_folder
+echo -e "script starting executing at :$G $(date)$N"| tee -a $log_file
+if [ $userid -ne 0 ]
+then
+    echo "please run this script with root access ">>$log_file
+    exit 1
+else
+    echo -e "running this script with $G root access $N" | tee -a $log_file
+fi
+
+VALIDATE(){
+    if [ $1 -eq 0 ] 
+    then
+        echo -e " $2 is $G success $N">>$log_file
+    else
+        echo -e " $2 is $R failure $N">>$log_file
+        exit 1
+    fi
+}
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+VALIDATE $? "copying mongo repo"
+
+dnf install mongodb-org -y 
+VALIDATE $? "mongobd installation"
+
+systemctl enable mongod
+VALIDATE $? "mongodb enabling"
+
+systemctl start mongod
+VALIDATE $? "mongodb starting"
+
+sed -i "s/127.0.0.1/0.0.0.0/g" /etc/mongod.conf
+VALIDATE $? "editing the mongodb conf for remote connectins"
+
+systemctl restart mongod
+VALIDATE $? "mongodb restarting"
