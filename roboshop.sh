94319cd3ca75287619fb6cb592a5a121f4ade6b0
sabeerabegumsayyad-max <sabeerabegumsayyad@gmail.com>
2026-03-01 00:09:20 +0530

    instances are creating

--- roboshop.sh
+++ roboshop.sh
@@ -6,15 +6,27 @@ INSTANCES=("mongodb" "mysql" "rabitmq" "payment" "shipping" "cart" "user" "catal
 ZONE_ID="Z05998412DIKACIE1APGQ"
 DOMAIN_NAME="sabeera.online"
 
-for instance in ${INSTANCES[@]}
+for instance in "${INSTANCES[@]}"
 do
     Instance_ID=$(aws ec2 run-instances \
-    --image-id ami-0220d79f3f480ecf5 \  --instance-type t2.micro \  --security-group-ids sg-0dc0951f043f884d8 \  --tag-specifications "ResourseTyoe=instance,Tags=[{ key=Name, Value=$instance }]" \  --query 'Instances[0].InstanceID' \  --output text)
-    if [ $instances -ne "frontend" ]
-    then
-        IP=$(aws ec2 describe-instances \  --instance-ids $INSTANCE_ID \  --query 'Reservations[0].Instances[0].PrivateIpAddress' \  --output text)
+        --image-id ami-0220d79f3f480ecf5 \
+        --instance-type t2.micro \
+        --security-group-ids sg-0dc0951f043f884d8 \
+        --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
+        --query 'Instances[0].InstanceId' \
+        --output text)
+
+    if [ "$instance" != "frontend" ]; then
+        IP=$(aws ec2 describe-instances \
+            --instance-ids "$Instance_ID" \
+            --query 'Reservations[0].Instances[0].PrivateIpAddress' \
+            --output text)
     else
-        IP=$(aws ec2 describe-instances \  --instance-ids $INSTANCE_ID \  --query 'Reservations[0].Instances[0].PublicIpAddress' \  --output text)
+        IP=$(aws ec2 describe-instances \
+            --instance-ids "$Instance_ID" \
+            --query 'Reservations[0].Instances[0].PublicIpAddress' \
+            --output text)
     fi
-    echo "$instance IP adress : $IP"
+
+    echo "$instance IP address: $IP"
 done
