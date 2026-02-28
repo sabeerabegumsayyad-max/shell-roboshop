1ad7ed4a97bd262c8ad34e6f2b807a10bacd9004
sabeerabegumsayyad-max <sabeerabegumsayyad@gmail.com>
2026-03-01 00:05:25 +0530

    instances are creating

--- roboshop.sh
+++ roboshop.sh
@@ -10,7 +10,7 @@ for instance in ${INSTANCES[@]}
 do
     Instance_ID=$(aws ec2 run-instances \
     --image-id ami-0220d79f3f480ecf5 \  --instance-type t2.micro \  --security-group-ids sg-0dc0951f043f884d8 \  --tag-specifications "ResourseTyoe=instance,Tags=[{ key=Name, Value=$instance }]" \  --query 'Instances[0].InstanceID' \  --output text)
-    if [ $instances != "frontend" ]
+    if [ $instances -ne "frontend" ]
     then
         IP=$(aws ec2 describe-instances \  --instance-ids $INSTANCE_ID \  --query 'Reservations[0].Instances[0].PrivateIpAddress' \  --output text)
     else
