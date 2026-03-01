b3e2db007fdfe33cc66896112ca78d27db39d4d2
sabeerabegumsayyad-max <sabeerabegumsayyad@gmail.com>
2026-03-01 16:08:49 +0530

    created instances

--- frontend.sh
+++ frontend.sh
@@ -32,6 +32,9 @@ VALIDATE(){
     fi
 }
 
+#!/bin/bash
+
+# 1. Setup Nginx Module and Install
 dnf module disable nginx -y &>>$log_file
 VALIDATE $? "disabling nginx"
 
@@ -41,23 +44,29 @@ VALIDATE $? "enabling nginx"
 dnf install nginx -y &>>$log_file
 VALIDATE $? "installing nginx"
 
+# 2. Enable and Start Service
 systemctl enable nginx &>>$log_file
-systemctl start nginx 
+# FIX: Added log redirection here to match the rest of the script
+systemctl start nginx &>>$log_file
 VALIDATE $? "starting nginx"
 
+# 3. Remove Default Content
 rm -rf /usr/share/nginx/html/* &>>$log_file
 VALIDATE $? "removing existing data"
 
+# 4. Download and Extract Code
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip &>>$log_file
 VALIDATE $? "downloading code"
 
-cd /usr/share/nginx/html
-unzip /tmp/frontend.zip &>>$log_file
+# FIX: Use -d flag to extract directly to the folder, avoiding 'cd'
+unzip /tmp/frontend.zip -d /usr/share/nginx/html/ &>>$log_file
 VALIDATE $? "unzipping code"
-#there will have some defaulut contetnt tyt that one we need to remove
+
+# 5. Configure Nginx
 rm -rf /etc/nginx/nginx.conf &>>$log_file
 VALIDATE $? "remove default nginx"
 
+# Ensure script_dir is defined (e.g., export script_dir=/path/to/script)
 cp $script_dir/nginx.conf /etc/nginx/nginx.conf &>>$log_file
 VALIDATE $? "copying nginx.conf"
 
